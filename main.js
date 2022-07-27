8054365dba54b5ef1dfa152ebbc3e206e20179c3
AhmedReFu <mdnazmulkhanfeni@gmail.com>
2022-07-27 19:48:35 +0600

    final draft

---
+++ main.js
@@ -0,0 +1,13 @@
+/* 
+document
+    .getElementById("upmatch")
+    .addEventListener("click", function recordCountHandler(e) {
+        document.getElementById("match").style.display = "none";
+    });
+
+
+document
+    .getElementById("blog")
+    .addEventListener("click", function recordCountHandler(e) {
+        document.getElementById("match").style.display = "block";
+    }); */
